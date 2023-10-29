bbc8f00cdb34bd53cadb7957cb6df6f3fce983d3
elimanaj <35267059+elimanaj@users.noreply.github.com>
2023-10-29 13:56:14 -0400

    Review01 Part2

--- src/test/java/com/neotech/steps/AddEmployeeSteps.java
+++ src/test/java/com/neotech/steps/AddEmployeeSteps.java
@@ -14,10 +14,18 @@ public class AddEmployeeSteps extends CommonMethods {
 		click(dashboard.addEmployeeLink);
 	}
 
+	// Hard-coded step
 	@When("user enters employee first name and last name")
 	public void user_enters_employee_first_name_and_last_name() {
 		sendText(addEmployee.firstName, "Lionel");
 		sendText(addEmployee.lastName, "Messi");
+	}
+
+	// Parameterized step
+	@When("user enters employee first name {string} and last name {string}")
+	public void user_enters_employee_first_name_and_last_name(String firstName, String lastName) {
+		sendText(addEmployee.firstName, firstName);
+		sendText(addEmployee.lastName, lastName);
 	}
 
 	@When("user selects a location")
@@ -31,6 +39,7 @@ public class AddEmployeeSteps extends CommonMethods {
 		click(addEmployee.saveButton);
 	}
 
+	// Hard-coded Validation
 	@Then("validate that employee is added successfully")
 	public void validate_that_employee_is_added_successfully() {
 		waitForVisibility(personalDetails.personalDetailsForm);
@@ -41,5 +50,31 @@ public class AddEmployeeSteps extends CommonMethods {
 		// Please make sure you import the Assert class under org.junit package
 		Assert.assertEquals("The employee name DOES NOT match!", expected, actual);
 	}
+
+	// Parameterized Validation
+	@Then("validate that employee {string} is added successfully")
+	public void validate_that_employee_is_added_successfully(String expectedName) {
+		waitForVisibility(personalDetails.personalDetailsForm);
+
+		String actualName = personalDetails.employeeName.getText();
+
+		// Please make sure you import the Assert class under org.junit package
+		Assert.assertEquals("The employee name DOES NOT match!", expectedName, actualName);
+	}
+
+	@When("user clicks on Create Login Details")
+	public void user_clicks_on_create_login_details() {
+		jsClick(addEmployee.loginDetailsToggle);
+	}
+
+	@When("user provides credentials")
+	public void user_provides_credentials() {
+		sendText(addEmployee.username, "MarioIcardi");
+		sendText(addEmployee.password, "Wanda@123");
+		sendText(addEmployee.confirmPassword, "Wanda@123");
+	}
+
+	// Homework for Hard working students
+	// Do the step above with parameterized username and password
 
 }
