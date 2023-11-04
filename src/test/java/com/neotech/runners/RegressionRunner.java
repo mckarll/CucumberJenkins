57bb06f8fa8e42258123c2b41b28c3a2d5c72c72
elimanaj <35267059+elimanaj@users.noreply.github.com>
2023-11-04 14:39:00 -0400

    Lesson06

---
+++ src/test/java/com/neotech/runners/RegressionRunner.java
@@ -0,0 +1,27 @@
+package com.neotech.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "src/test/resources/features/",
+		glue = "com.neotech.steps",
+		dryRun = false,
+		tags = "@regression",
+		monochrome = true,
+		
+		plugin = {
+				// "pretty",
+				"html:target/cucumber-default-report.html",
+				"json:target/cucumber.json",
+				"rerun:target/failed.txt"
+				}
+		)
+
+public class RegressionRunner {
+
+}
